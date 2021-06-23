8f88c3af162a340cd407c75f801ba5f97a95c7f8
Steve Corwin <37982371+PrinceCorwin@users.noreply.github.com>
2021-06-23 12:22:28 -0500

    logic seems to be working
    Will test more and add style

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 // algorithm
-// event listener for any key
-
+// global variables
+let counter = 0;
+let colorArray = [];
+let audioArray = [];
+let allColors = ["green", "red", "yellow", "blue"];
+let allSounds = [
+  "./sounds/green.mp3",
+  "./sounds/red.mp3",
+  "./sounds/yellow.mp3",
+  "./sounds/blue.mp3",
+];
 // preload audio files to negate delay
 let audio = new Audio("./sounds/green.mp3");
-audio = new Audio("./sounds/blue.mp3");
-audio = new Audio("./sounds/yellow.mp3");
-audio = new Audio("./sounds/red.mp3");
-audio = new Audio("./sounds/wrong.mp3");
-
-initialize();
-// create empty colorArray and audioArray
-function initialize() {
-  let colorArray = [];
-  let audioArray = [];
-  let allColors = ["green", "red", "yellow", "blue"];
-  let allSounds = [
-    "./sounds/green.mp3",
-    "./sounds/red.mp3",
-    "./sounds/yellow.mp3",
-    "./sounds/blue.mp3",
-  ];
-  $("h1").click(function () {
-    // remove event listener
-    $("body").off("keydown");
-    setTimeout(() => {
-      simonPlay(colorArray, audioArray, allColors, allSounds);
-    }, 500);
+// audio = new Audio("./sounds/blue.mp3");
+// audio = new Audio("./sounds/yellow.mp3");
+// audio = new Audio("./sounds/red.mp3");
+// audio = new Audio("./sounds/wrong.mp3");
+function listenForStart() {
+  $("#title").on("click", function () {
+    $("#title").off("click");
+    simonPlay();
   });
 }
+listenForStart();
 
+function listenForPlayer() {
+  $("button").click(function (event) {
+    playerPlay(event);
+  });
+}
 // simonPlay function
-function simonPlay(colorArray, audioArray, allColors, allSounds, counter) {
+function simonPlay() {
+  counter = 0;
+
   let rndNum = Math.floor(Math.random() * 4);
 
   // push random color to colorArray and audioArray
@@ -50,43 +52,52 @@ function simonPlay(colorArray, audioArray, allColors, allSounds, counter) {
         $("." + colorArray[i]).removeClass("pressed");
       }, 200);
     }, 500 * i + 500);
+    if (i === colorArray.length - 1) {
+      setTimeout(() => {
+        listenForPlayer();
+      }, 500 * i + 500);
+    }
   }
-  playerPlay(colorArray, audioArray, allColors, allSounds);
 }
 
 //   player's turn
-function playerPlay(colorArray, audioArray, allColors, allSounds) {
-  var counter = 0;
+function playerPlay(event) {
+  console.log("event id: " + event.target.id);
+  $("#" + event.id).addClass("pressed");
+  setTimeout(() => {
+    $("#" + event.target.id).removeClass("pressed");
+  }, 200);
+  if (event.target.id === colorArray[counter]) {
+    audio = new Audio("./sounds/" + event.target.id + ".mp3");
+    audio.play();
+  } else {
+    audio = new Audio("./sounds/wrong.mp3");
+    audio.play();
+    gameOver();
+    return;
+  }
+  console.log("counter: " + counter);
+  console.log(colorArray.length - 1);
+  if (counter === colorArray.length - 1) {
+    $("button").off("click");
 
-  $("button").click(function (event) {
-    console.log("event id: " + event.target.id);
-    $("#" + event.id).addClass("pressed");
-    setTimeout(() => {
-      $("#" + event.target.id).removeClass("pressed");
-    }, 200);
-    if (event.target.id === colorArray[counter]) {
-      audio = new Audio("./sounds/" + event.target.id + ".mp3");
-      audio.play();
-    } else {
-      audio = new Audio("./sounds/wrong.mp3");
-      audio.play();
-      gameOver();
-      return;
-    }
-    console.log("counter: " + counter);
-    console.log(colorArray.length - 1);
-    if (counter === colorArray.length - 1) {
-      simonPlay(colorArray, audioArray, allColors, allSounds);
-      //   return;
-    }
-    counter++;
-    // return;
-  });
+    simonPlay();
+    return;
+  }
+  counter++;
+  // return;
 }
+
 // GAME OVER FUNCTION
 function gameOver() {
-  console.log("game Over");
-  initialize();
+  $("button").off("click");
+  counter = 0;
+  colorArray = [];
+  audioArray = [];
+  $("#title").on("click", function () {
+    $("#title").off("click");
+    simonPlay();
+  });
 }
 // show player stats
 // run keyListen
